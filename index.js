2d7304782bbd85130c7d78bbf2e100df088ba8e3
JyotirmoyGupta <107062140+JyotirmoyGupta@users.noreply.github.com>
2022-06-23 22:59:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var superheroes = require("superheroes");
+
+var mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
+
+var character = require("marvel-characters");
+
+var character = marvel_characters.random();
+
+console.log(character());
+
+console.log(character);
+
+var villain = require("supervillains");
+
+randomSupervillain = villain.random();
+
+randomSupervillainAgain = villain.all;
+console.log(randomSupervillain);
+
+console.log(randomSupervillainAgain);
